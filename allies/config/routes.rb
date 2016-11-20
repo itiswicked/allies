6029ef2fc38104a7978bb0073e167cc895ffab8d
Nathan Bourke <nathanrbourke@gmail.com>
2016-11-20 11:10:15 -0500

    create requests show page

--- allies/config/routes.rb
+++ allies/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :ratings, only: [:new, :create]
   resources :organizations
+  resources :requests, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

44494980bb45ccb8dad8760e8cef5e180135b5c4
lilybarrett <lily@barrett.us>
2016-11-20 12:26:14 -0500

    Style splash page, add root path to splash

--- allies/config/routes.rb
+++ allies/config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
       resources :allies
     end
   end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
